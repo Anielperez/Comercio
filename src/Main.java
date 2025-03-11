98bdc01a1b25ae10e17592e6e8b4ebb78d699bf0
Anielperez <193621872+Anielperez@users.noreply.github.com>
2025-03-10 21:40:40 -0500

    construccion del back-end

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+
+        // Proyect  for E-commerce
+        List<String> cities = new ArrayList<>();
+        cities.add("Londres");
+        cities.add("Tokyo");
+        cities.add("Barcelona");
+        cities.add("Buenos Aires ");
+        cities.add("Bogota");
+        cities.add("Ciudad de Mexico");
+        cities.add("Lima");
+
+        /*for (int i = 0; i < cities.size(); i++){
+
+            System.out.println(cities.get(i));
+        }*/
+
+        /*for (String city : cities) {
+            System.out.println(city);
+        }*/
+        
+
+    }
     }
 
-    // Proyect  for E-commerce
-
-
-    @Override
-    public String toString() {
-        return super.toString();
-    }
-}
